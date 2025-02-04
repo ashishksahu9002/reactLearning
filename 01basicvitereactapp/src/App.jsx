a0504b0445addf532a2d416b86dc2abd5413088e
Ashish Kumar <ashishksahu9002@gmail.com>
2025-02-04 11:09:35 +0530

    06 currency converter

---
+++ 01basicvitereactapp/src/App.jsx
@@ -0,0 +1,17 @@
+import Chai from "./Chai"
+function App() {
+
+  return (
+    <>
+      <Chai />
+      <h1>Hello World Vite</h1>
+    </>
+  )
+}
+
+export default App
+
+
+/*
+ In vite component should be named in capital and that file should be saved with .jsx extension
+*/
